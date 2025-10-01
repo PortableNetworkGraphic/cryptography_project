88b7907a65d6c326dffa0e70a26d4f06c0efea36
Sucrose.PNG <ceilnicierega+SucrosePNG@proton.me>
2025-10-01 22:38:13 +0100

    Made a beginning on comments for the padding file. TBC. :3

--- primitives/padding.py
+++ primitives/padding.py
@@ -1,11 +1,21 @@
 from random import getrandbits
 
+# The subroutines found in this file pad a stream of bytes to a number of bytes such that it is a multiple of the
+# block size and can be feasibly unpadded to return the initial byte stream.
+
+# Pads accoring to PKCS#7 padding.
 def PKCS_7_pad(data: bytes, block_size: int) -> bytes:
+
+    # Calculates number of additionaly bytes required before padding this number as a byte an appropriate number of
+    # times.
     n: int = block_size - (len(data) % block_size)
     data += bytes([n]) * n
     return data
 
+# Unpads to undo PKCS#7 padding.
 def PKCS_7_unpad(data: bytes, block_size: int) -> bytes:
+
+    # The last number
     n: int = data[-1]
     if n < 1 or n > len(data) or n > block_size:
         raise ValueError("Invalid Padding Length")
@@ -13,11 +23,13 @@ def PKCS_7_unpad(data: bytes, block_size: int) -> bytes:
         raise ValueError("Invalid Padding Sequence")
     return data[:-n]
 
+# Pads accoring to ANSI X9.23 padding.
 def ANSI_X9_23_pad(data: bytes, block_size: int) -> bytes:
     n: int = block_size - (len(data) % block_size)
     data += b'\x00' * (n-1) + bytes([n])
     return data
 
+# Unpads to undo ANSI X9.23 padding.
 def ANSI_X9_23_unpad(data: bytes, block_size: int) -> bytes:
     n: int = data[-1]
     if n < 1 or n > len(data) or n > block_size:
@@ -26,22 +38,26 @@ def ANSI_X9_23_unpad(data: bytes, block_size: int) -> bytes:
         raise ValueError("Invalid Padding Sequence")
     return data[:-n]
 
+# Pads accoring to ISO 10126 padding.
 def ISO_10126_pad(data: bytes, block_size: int) -> bytes:
     n: int = block_size - (len(data) % block_size)
     data += bytes([getrandbits(8) for i in range(n-1)]) + bytes([n])
     return data
 
+# Unpads to undo ISO 10126 padding.
 def ISO_10126_unpad(data: bytes, block_size: int) -> bytes:
     n: int = data[-1]
     if n < 1 or n > len(data) or n > block_size:
         raise ValueError("Invalid Padding Length")
     return data[:-n]
 
+# Pads accoring to ISO/IEC 7816-4:2005 padding.
 def ISO_7816_4_pad(data: bytes, block_size: int) -> bytes:
     n: int = block_size - (len(data) % block_size)
     data += b"\x80"+b"\x00"*(n-1)
     return data
 
+# Unpads to undo ISO/IEC 7816-4:2005 padding.
 def ISO_7816_4_unpad(data: bytes, block_size: int) -> bytes:
     c = 0
     while data[-1] != 0x80:
@@ -51,6 +67,7 @@ def ISO_7816_4_unpad(data: bytes, block_size: int) -> bytes:
             raise ValueError("Invalid Padding Length")
     return data[:-1]
 
+# Pads accoring to SHA-512 padding.
 def SHA_pad(data: int) -> int:
     l: int = data.bit_length()
     if l >= (1 << 64):

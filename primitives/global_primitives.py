aa6d1bfb81be52a5de3a9992b4ad281fd995a741
Fructose.PNG <ceilnicierega+fructosePNG@proton.me>
2025-10-23 14:00:17 +0100

    Further work on extended euclidean algorithms and modular inverse.

--- primitives/global_primitives.py
+++ primitives/global_primitives.py
@@ -1,3 +1,20 @@
+import math, egcd, random, sympy
+
+def TEMPegcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = TEMPegcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def TEMPmodinv(a, m):
+    g, x, y = extended_euclidean_algorithm(a, m)
+    if g != 1:
+        print(a, m)
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
 def euclidean_algorithm(a: int, b: int) -> int:
     if a < 0 or b < 0: raise ValueError("Extended Euclidean Algorithm: Inputs must be non-negative")
     if a < b:
@@ -65,8 +82,32 @@ def extended_euclidean_algorithm_x(a: int, b: int) -> int:
     return x2
 
 def modular_multiplicative_inverse(a: int, p: int, q: int) -> int:
+
+    # Calculates lambdaN knowing p and q
     lambdaN = ((p-1) * (q-1)) // euclidean_algorithm(p-1, q-1)
+
+    # Uses lambda n to find the multiplicative inverse of the input a mod pq
     d = extended_euclidean_algorithm(a, lambdaN)[1]
+
+    # Account for f d is negative
     return d if d >= 0 else d + lambdaN
 
-print(modular_multiplicative_inverse(17, 61, 53))
+ts = set()
+
+# e = 65537 = 2^16 + 1
+e = 2**16+1
+for i in range(100000):
+
+    lambdaN, p, q = 0, None, None
+
+    while lambdaN < e or euclidean_algorithm(e, lambdaN) != 1:
+        # Generate random prime numbers
+        p = sympy.randprime(0, 2 ** 20)
+        q = sympy.randprime(0, 2 ** 20)
+
+        lambdaN = ((p-1) * (q-1)) // euclidean_algorithm(p-1, q-1)
+
+    # Compares my own calculated value to an assumed valid calculator for the inverse mod lambdaN
+    ts.add(modular_multiplicative_inverse(e, p, q) == TEMPmodinv(e, lambdaN))
+
+print(ts)
